dfb18774c22458d4f70172e28ffe187e109de706
matteo <matteo.ginetti99@gmail.com>
2020-01-12 12:48:23 +0100

    Argumets new changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const arguments = document.getElementsByClassName('argument');
+const arguments = document.getElementsByClassName('.arguments argument');
 
 arguments[0].style.marginLeft = '40px';
 arguments[arguments.length-1].style.marginRight = '40px';
