33078966f9bfc820fd1f49d193a76a7d0fb0a4f7
matteo <matteo.ginetti99@gmail.com>
2020-01-15 16:40:19 +0100

    Fix app.js

--- js/app.js
+++ js/app.js
@@ -6,57 +6,56 @@ if (arguments[0]) {
 	arguments[arguments.length - 1].style.marginRight = "40px";
 }
 
+const createCoordinate = () => {
+	let to_return = {};
+	to_return.x = Math.round(Math.random() * 100);
+	while (to_return.x < 44) {
+		to_return.x = Math.round(Math.random() * 100);
+	}
+	to_return.y = Math.floor(Math.random() * 1000) + 1;
+	while (to_return.y < 44 || to_return.y > window.innerHeight/2) {
+		to_return.y = Math.floor(Math.random() * 1000) + 1;
+	}
+	to_return.finalX = Math.floor(Math.random() * 1000) + 1;
+	while (
+		to_return.finalX < window.innerWidth / 2 ||
+		to_return.finalX > window.innerWidth - 44
+	) {
+		to_return.finalX = Math.floor(Math.random() * 1000) + 1;
+	}
+	return to_return;
+};
+
 const fishAnimation = () => {
-	console.log(window.innerWidth);
 	const refreshRate = 1000 / 60;
-	const maxXPosition = window.innerWidth;
 	let rect = document.getElementById("fish");
-	let speedX = 1;
-	let x = generateRandomX();
-	while (x > maxXPosition) {
-		x = generateRandomX();
-	}
-	let positionX = generateRandomX();
-	while (x > positionX || positionX > maxXPosition - 44) {
-		positionX = generateRandomX();
-	}
-	let y = generateRandomX();
-	while (y > window.innerHeight - 44) {
-		y = generateRandomX();
-	}
-	let positionY = generateRandomX();
-	while (y > positionY || positionY > innerHeight) {
-		positionY = generateRandomX();
-	}
-	let pos = x;
-	let posY = y;
-	let rotate = true;
-	console.log(rect.children);
+	let speed = 1;
+	let coordinate = createCoordinate();
+	let pos = coordinate.x;
+	let posY = coordinate.y;
 	window.setInterval(() => {
-		pos = pos + speedX;
-		posY = posY + speedX;
-		if (pos > positionX || pos < x || posY > positionY || posY < y) {
-			speedX = speedX * -1;
-			console.log(rect.classList)
-			if(rect.classList[1] == 'left'){
-				rect.classList.remove('left')
-				rect.classList.add('right')
+		pos = pos + speed;
+		posY = posY + speed;
+		if (
+			pos > coordinate.finalX ||
+			posY > window.innerHeight-44 ||
+			pos < coordinate.x ||
+			posY < coordinate.y
+		) {
+			speed = speed * -1;
+			if (rect.classList[1] == "left") {
+				rect.classList.remove("left");
+				rect.classList.add("right");
 			} else {
-				rect.classList.remove('right')
-				rect.classList.add('left')
+				rect.classList.remove("right");
+				rect.classList.add("left");
 			}
-			console.log('qui')
 			rotate = false;
 		}
 
 		rect.style.left = pos + "px";
 		rect.style.top = posY + "px";
 	}, refreshRate);
-};
-
-const generateRandomX = () => {
-	let n = Math.round(Math.random() * 1000);
-	return n;
 };
 
 fishAnimation();
