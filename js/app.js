090716e295def428f355ce736e9fd431a101ad82
matteo <matteo.ginetti99@gmail.com>
2020-01-19 16:05:25 +0100

    New animation

--- js/app.js
+++ js/app.js
@@ -1,61 +1,61 @@
 const arguments = document.getElementsByClassName(".arguments argument");
 const jumbotron = document.getElementById("jumbotron");
 
+
+const animation = (fish, x, y) => {
+
+    const refreshrate = 1000 / 60;
+    const speed = 1;
+    let pos = x;
+    let posY = y
+    const interval = window.setInterval(() => {
+        pos += speed;
+        posY -= speed/3
+        if (pos > window.innerWidth - (window.innerWidth / 100) * 5 || posY < 0)  {
+            fish.parentNode.removeChild(fish);
+            generateFish();
+            clearInterval(interval);
+        }
+        fish.style.left = pos + "px";
+        fish.style.top = posY + "px";
+    }, refreshrate);
+}
+
+const generateXY = (fish, id) => {
+    const generateNumber = () => {
+        return Math.floor(Math.random() * 1000);
+    };
+    let x = generateNumber();
+    let y = generateNumber();
+
+    while (x > window.innerWidth / 3) {
+        x = generateNumber();
+    }
+    while (y > window.innerHeight - 44 || y < 44) {
+        y = generateNumber();
+    }
+
+    fish.style.top = y + "px";
+    fish.style.left = x + "px";
+
+    jumbotron.appendChild(fish);
+
+    animation(fish, x, y)
+};
+
+const generateFish = () => {
+    let fish = document.createElement("div");
+    fish.classList.add("fish");
+    fish.id = "fish1";
+
+    generateXY(fish, 1);
+};
+
 if (arguments[0]) {
 	arguments[0].style.marginLeft = "40px";
 	arguments[arguments.length - 1].style.marginRight = "40px";
 }
 
-const createCoordinate = () => {
-	let to_return = {};
-	to_return.x = Math.round(Math.random() * 100);
-	while (to_return.x < 44) {
-		to_return.x = Math.round(Math.random() * 100);
-	}
-	to_return.y = Math.floor(Math.random() * 1000) + 1;
-	while (to_return.y < 44 || to_return.y > window.innerHeight/2) {
-		to_return.y = Math.floor(Math.random() * 1000) + 1;
-	}
-	to_return.finalX = Math.floor(Math.random() * 1000) + 1;
-	while (
-		to_return.finalX < window.innerWidth / 2 ||
-		to_return.finalX > window.innerWidth - 44
-	) {
-		to_return.finalX = Math.floor(Math.random() * 1000) + 1;
-	}
-	return to_return;
-};
-
-const fishAnimation = () => {
-	const refreshRate = 1000 / 60;
-	let rect = document.getElementById("fish");
-	let speed = 1;
-	let coordinate = createCoordinate();
-	let pos = coordinate.x;
-	let posY = coordinate.y;
-	window.setInterval(() => {
-		pos = pos + speed;
-		posY = posY + speed;
-		if (
-			pos > coordinate.finalX ||
-			posY > window.innerHeight-44 ||
-			pos < coordinate.x ||
-			posY < coordinate.y
-		) {
-			speed = speed * -1;
-			if (rect.classList[1] == "left") {
-				rect.classList.remove("left");
-				rect.classList.add("right");
-			} else {
-				rect.classList.remove("right");
-				rect.classList.add("left");
-			}
-			rotate = false;
-		}
-
-		rect.style.left = pos + "px";
-		rect.style.top = posY + "px";
-	}, refreshRate);
-};
-
-fishAnimation();
+for (let i = 0; i < 10; i++) {
+    generateFish();
+}
